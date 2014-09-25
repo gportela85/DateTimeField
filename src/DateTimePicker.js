3f932f618b22c59e091f02cddf739e8ef95ebfcd
Guilherme Lopes Portela <guilherme@gmwsolucoes.com.br>
2014-09-25 04:47:37 -0300

    Fixed issue with Month/Year calendar collapsing by itself.

--- src/DateTimePicker.js
+++ src/DateTimePicker.js
@@ -252,6 +252,76 @@
             me.timePicker.show();
         },1);
     },
+    runAnimation: function(isHide){
+      if(Ext.getVersion().major == 5) {
+        var me = this,
+            picker = this.monthPicker,
+            options = {
+                duration: 200,
+                callback: function() {
+                    picker.setVisible(!isHide);
+                    // See showMonthPicker
+                    picker.ownerCmp = isHide ? null : me;
+                }
+            };
+
+        if (isHide) {
+            picker.el.slideOut('t', options);
+        } else {
+            picker.el.slideIn('t', options);
+        }
+      } else {
+        this.callParent(arguments);
+      }
+    },
+    hideMonthPicker: function(animate){
+      if(Ext.getVersion().major == 5) {
+        var me = this,
+            picker = me.monthPicker;
+
+        if (picker && picker.isVisible()) {
+            if (me.shouldAnimate(animate)) {
+                me.runAnimation(true);
+            } else {
+                picker.hide();
+                // See showMonthPicker
+                picker.ownerCmp = null;
+            }
+        }
+        return me;
+      } else {
+        this.callParent(arguments);
+      }
+    },
+    showMonthPicker: function(animate) {
+      if(Ext.getVersion().major == 5) {
+        var me = this,
+            el = me.el,
+            picker;
+        
+        if (me.rendered && !me.disabled) {
+            picker = me.createMonthPicker();            
+            if (!picker.isVisible()) {
+                picker.setValue(me.getActive());
+                picker.setSize(el.getSize());
+                picker.setPosition(-el.getBorderWidth('l'), -el.getBorderWidth('t'));
+                if (me.shouldAnimate(animate)) {
+                    me.runAnimation(false);
+                } else {
+                    picker.show();
+                    // We need to set the ownerCmp so that owns() can correctly
+                    // match up the component hierarchy, however when positioning the picker
+                    // we don't want it to position like a normal floater because we render it to 
+                    // month picker element itself.
+                    picker.ownerCmp = me;
+                }
+            }
+        }
+        return me;
+      } else {
+        this.callParent(arguments);
+      }
+    },
     onHide: function () {
         var me = this;
         me.timePicker.hide();
